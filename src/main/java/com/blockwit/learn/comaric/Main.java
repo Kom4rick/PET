3da3bb76851d561d6ccaf74532af477a4faa888f
Mark Terekhov <forsenboy123@gmail.com>
2023-02-05 20:05:14 +0300

    Post request

--- src/main/java/com/blockwit/learn/comaric/Main.java
+++ src/main/java/com/blockwit/learn/comaric/Main.java
@@ -6,19 +6,30 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+//import java.com.blockwit.learn.comaric;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://w3dna.net/"))
+                .uri(URI.create("https://w3dna.net/api/sales/domains/price"))
                 .version(HttpClient.Version.HTTP_2)
                 .GET()
                 .header("123","123")
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.statusCode());
+        //System.out.println(response.statusCode());
+        //System.out.println(response.body());
+        HttpRequest request1 = HttpRequest.newBuilder()
+                .uri(URI.create("https://w3dna.net/api/sales/domains/price"))
+                .version(HttpClient.Version.HTTP_2)
+                .POST(HttpRequest.BodyPublishers.ofString("\"123\""))
+                .build();
+        HttpResponse<String> response1 =client.send(request1,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response1.body());
+
+
     }
 
 }
