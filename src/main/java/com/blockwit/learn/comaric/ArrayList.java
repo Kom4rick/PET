9d5bc61dfbd9e6ec9a5ec194d654a25f5b2bbcc9
Mark Terekhov <forsenboy123@gmail.com>
2023-02-08 15:13:07 +0300

    Update ArrayList

--- src/main/java/com/blockwit/learn/comaric/ArrayList.java
+++ src/main/java/com/blockwit/learn/comaric/ArrayList.java
@@ -1,2 +1,28 @@
-package com.blockwit.learn.comaric;public class ArrayList {
+package com.blockwit.learn.comaric;
+
+import java.util.Arrays;
+
+public class ArrayList {
+    String temp [] = new String[1];
+    private int size = 0;
+
+    public ArrayList(String [] temp){
+        this.temp = temp;
+
+    }
+
+    public void add(String element){
+        if (temp.length == size){
+            String[] temp1 = Arrays.copyOf(temp, temp.length+1);
+            temp = new String[temp1.length];
+            temp = temp1;
+            temp[temp.length-1] = element;
+        }
+        size++;
+
+    }
+    public String getElement(int index){
+
+        return temp[index];
+    }
 }
--- src/main/java/com/blockwit/learn/comaric/Main.java
+++ src/main/java/com/blockwit/learn/comaric/Main.java
@@ -1,5 +1,7 @@
 package com.blockwit.learn.comaric;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -12,23 +14,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://w3dna.net/api/sales/domains/price"))
-                .version(HttpClient.Version.HTTP_2)
-                .GET()
-                .header("123","123")
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        //System.out.println(response.statusCode());
-        //System.out.println(response.body());
         HttpRequest request1 = HttpRequest.newBuilder()
                 .uri(URI.create("https://w3dna.net/api/sales/domains/price"))
-                .version(HttpClient.Version.HTTP_2)
-                .POST(HttpRequest.BodyPublishers.ofString("\"123\""))
+                .version(HttpClient.Version.HTTP_1_1)
+                .POST(HttpRequest.BodyPublishers.ofString("{\"domainNames\":[\"123\"]}"))
+                .header("Content-Type", "application/json")
                 .build();
-        HttpResponse<String> response1 =client.send(request1,HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
+
         System.out.println(response1.body());
 
+        String b[] = new String[0];
+        ArrayList a = new ArrayList(b);
+        a.add("pf");
+        a.add("13");
+        a.add("asd");
+        System.out.println(a.getElement(2));
 
     }
 
