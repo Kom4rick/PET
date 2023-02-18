0309a3711ba841bc67a49869c601d033faa72920
Alexander Strakh <cromlehg@gmail.com>
2023-02-18 15:26:20 +0300

    fix: imports

--- src/main/java/com/blockwit/learn/comaric/Main.java
+++ src/main/java/com/blockwit/learn/comaric/Main.java
@@ -1,43 +1,39 @@
 package com.blockwit.learn.comaric;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-//import java.com.blockwit.learn.comaric;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request1 = HttpRequest.newBuilder()
-                .uri(URI.create("https://w3dna.net/api/sales/domains/price"))
-                .version(HttpClient.Version.HTTP_1_1)
-                .POST(HttpRequest.BodyPublishers.ofString("{\"domainNames\":[\"123\"]}"))
-                .header("Content-Type", "application/json")
-                .build();
-        HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
+	public static void main(String[] args) throws IOException, InterruptedException {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request1 = HttpRequest.newBuilder()
+			.uri(URI.create("https://w3dna.net/api/sales/domains/price"))
+			.version(HttpClient.Version.HTTP_1_1)
+			.POST(HttpRequest.BodyPublishers.ofString("{\"domainNames\":[\"123\"]}"))
+			.header("Content-Type", "application/json")
+			.build();
+		HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response1.body());
-        long startTime = System.currentTimeMillis();
-        ArrayList a = new ArrayList();
-        for (int i = 0; i < 1_000_000; i++) {
-            a.add("");
-        }
-        long finishTime = System.currentTimeMillis();
-        System.out.println( finishTime-startTime );
+		System.out.println(response1.body());
+		long startTime = System.currentTimeMillis();
+		ArrayList a = new ArrayList();
+		for (int i = 0; i < 1_000_000; i++) {
+			a.add("");
+		}
+		long finishTime = System.currentTimeMillis();
+		System.out.println(finishTime - startTime);
 
-        Map abc = new Map();
-        abc.add("petya");
-        abc.add("vasiya");
-        abc.add("yulya");
-        abc.add("lox");
-        System.out.println(abc.getElement(10));
+		Map abc = new Map();
+		abc.add("petya");
+		abc.add("vasiya");
+		abc.add("yulya");
+		abc.add("lox");
+		System.out.println(abc.getElement(10));
 
 
-    }
+	}
 }
