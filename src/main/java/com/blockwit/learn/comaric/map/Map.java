47f58e5a879ad99b88a68fa3e130a51b9547792d
Mark Terekhov <forsenboy123@gmail.com>
2023-02-19 19:17:33 +0300

    Update class Map

--- src/main/java/com/blockwit/learn/comaric/map/Map.java
+++ src/main/java/com/blockwit/learn/comaric/map/Map.java
@@ -7,21 +7,30 @@ public class Map {
 	private String names[] = new String[1];
 	private int size = 0;
 
-	public void add(int number, String element) {
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] == number){
-				names[i] = element;
-			}
-		}
+	public Map(int [] numbers, String[] names){
+		this.names = names;
+		this.numbers = numbers;
+	}
+	public Map() {
+
+	}
+
+	public void add(int key, String value) {
+
 		if (numbers.length == size) {
 			numbers = Arrays.copyOf(numbers, numbers.length + 10);
 		}
 		if (names.length == size) {
 			names = Arrays.copyOf(names, names.length + 10);
-			names[names.length - 1] = element;
+			names[names.length - 1] = value;
 		}
-		numbers[size] = number;
-		names[size] = element;
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] == key){
+				names[i] = value;
+			}
+		}
+		numbers[size] = key;
+		names[size] = value;
 		size++;
 	}
 
@@ -34,9 +43,9 @@ public class Map {
 		}
 		return s;
 	}
-	public void remove(int number){
+	public void remove(int index){
 		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] == number){
+			if (numbers[i] == index){
 				numbers[i] = 0;
 				names[i] = null;
 			}
--- src/main/java/com/blockwit/learn/comaric/map/UnitTests.java
+++ src/main/java/com/blockwit/learn/comaric/map/UnitTests.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 public class UnitTests {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+
 		Map abc = new Map();
 		abc.add(123, "Sir gay");
 		abc.add(324, "tolik");
 		abc.add(456, "masha");
 		abc.add(678, "nikita");
+		System.out.println(abc.get(324));
 		abc.add(324, "ebolik");
 		abc.remove(456);
-
 		System.out.println(abc.get(324));
+		System.out.println(abc.get(456));
+		abc.add(456, "o5 masha");
 		System.out.println(abc.get(456));
 	}
 
