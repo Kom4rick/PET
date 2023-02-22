e20e74c79ee5c551b5207cff59c9cae42f912306
Mark Terekhov <forsenboy123@gmail.com>
2023-02-22 11:32:55 +0300

    Update method remove

--- src/main/java/com/blockwit/learn/comaric/map/Map.java
+++ src/main/java/com/blockwit/learn/comaric/map/Map.java
@@ -3,14 +3,11 @@ package com.blockwit.learn.comaric.map;
 import java.util.Arrays;
 
 public class Map {
-	private int numbers[] = new int[1];
-	private String names[] = new String[1];
+	private int numbers[] = new int[10];
+	private String names[] = new String[10];
 	private int size = 0;
 
-	public Map(int [] numbers, String[] names){
-		this.names = names;
-		this.numbers = numbers;
-	}
+
 	public Map() {
 
 	}
@@ -22,7 +19,6 @@ public class Map {
 		}
 		if (names.length == size) {
 			names = Arrays.copyOf(names, names.length + 10);
-			names[names.length - 1] = value;
 		}
 		for (int i = 0; i < numbers.length; i++) {
 			if (numbers[i] == key){
@@ -34,22 +30,44 @@ public class Map {
 		size++;
 	}
 
-	public String get(int number) {
-		String s = null;
+	public String get(int index) {
+		String s = "По заданному ключу не найдено значение";
 		for (int i = 0; i < numbers.length; i++) {
-			if (number == numbers[i]){
+			if (index == numbers[i]){
 				return names[i];
 			}
 		}
 		return s;
 	}
 	public void remove(int index){
+
 		for (int i = 0; i < numbers.length; i++) {
 			if (numbers[i] == index){
 				numbers[i] = 0;
 				names[i] = null;
+				String [] names1 = Arrays.copyOfRange(names, 0, i);
+				String [] names2 = Arrays.copyOfRange(names, i+1, names.length);
+				names = new String[names.length-1];
+				for (int j = 0; j <names1.length; j++) {
+					names[j] = names1[j];
+				}
+				for (int j = 0; j < names2.length; j++) {
+					names[j+i] = names2[j];
+				}
+				int [] numbers1 = Arrays.copyOfRange(numbers, 0, i);
+				int [] numbers2 = Arrays.copyOfRange(numbers, i+1, numbers.length);
+				numbers = new int[numbers.length-1];
+				for (int j = 0; j <numbers1.length; j++) {
+					numbers[j] = numbers1[j];
+				}
+				for (int j = 0; j < numbers2.length; j++) {
+					numbers[j+i] = numbers2[j];
+				}
+				size--;
+				break;
 			}
 		}
+
 	}
 }
 
